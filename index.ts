9927eb8bc5bf0e65837d21b5c472db1077be750c
Eduardo Born <nosachamos@gmail.com>
2017-03-13 16:01:50 -0500

    Making version 2.2.1 closure compiler-friendly by expanding wildcard exports.

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
-export * from './src/dispatcher';
-export * from './src/ng2';
-export * from './src/reducer';
-export * from './src/state';
-export * from './src/store';
-export * from './src/utils';
+export { Action, Dispatcher } from './src/dispatcher';
+export { INITIAL_REDUCER, INITIAL_STATE, _INITIAL_REDUCER, _INITIAL_STATE, _initialReducerFactory, _initialStateFactory,
+  _storeFactory, _stateFactory, _reducerFactory, provideStore, StoreModule } from './src/ng2';
+export { Reducer, ActionReducer } from './src/reducer';
+export { State } from './src/state';
+export { Store } from './src/store';
+export { combineReducers } from './src/utils';
